e90683db842c096ec9f0d10cb11871979a52ae67
Samin Ahmed <saminahmed1001@gmail.com>
2025-12-29 01:48:07 +0600

    Feat: add useState to count scores

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Cards from "./components/Cards";
 import ScoreBoard from "./components/scoreBoard";
+import { useState } from "react";
 
 export default function App() {
+  const [scores, setScores] = useState({ currentScore: 0, highestScore: 0 });
   return (
     <div>
-      <ScoreBoard></ScoreBoard>
-      <Cards></Cards>
+      <ScoreBoard scores={scores}></ScoreBoard>
+      <Cards scores={scores} setScores={setScores}></Cards>
     </div>
   );
 }
